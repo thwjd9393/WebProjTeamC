f9194d5f23e440b05678c5f5780ce4482f712b70
dragonechoi <vasco1379@naver.com>
2023-05-18 15:50:44 +0900

    update) team server sync

--- php/communityWrite.php
+++ php/communityWrite.php
@@ -1,21 +1,21 @@
 <?php
 // 데이터베이스 연결 설정
-$db = mysqli_connect('localhost', 'vasco1379', 'chldydtjr159!', 'vasco1379');
-
+$db = mysqli_connect('localhost', 'mrhisj23', 'hi23bye6!', 'mrhisj23');
+mysqli_query($db, "set names utf8"); //한글깨짐 방지
 // 연결 확인
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
 
 // POST 데이터 받아오기
-$title = $_POST['title'];
-$contents = $_POST['contents'];
-$name = $_POST['name'];
-$password = $_POST['password'];
-$date = date("YmdHis"); // 현재 날짜시간 가져오기
+$title = $_POST['cmtyTitle'];
+$contents = $_POST['cmtyContent'];
+$name = $_POST['cmtyNic'];
+$password = $_POST['cmtyPasswd'];
+
 
 // 사진 파일 업로드
-$file = $_FILES['image'];
+$file = $_FILES['cmtyImg'];
 $filename = $file['name'];
 $tmpName = $file['tmp_name'];
 $dstName = "./uploadimg/" . date('YmdHis') . $filename;
@@ -29,15 +29,15 @@ if (!in_array($extension, $allowedExtensions)) {
     
     // 데이터 삽입 로직을 추가해야 함
 } else {
-    echo "사진 업로드 실패";
+   
 }
 
 // 데이터 삽입 쿼리 작성
-$sql = "INSERT INTO community (no, title, contents, name, password, date, image, like_ctn, comment) VALUES (null, '$title', '$contents', '$name', '$password', '$date', '$dstName', 0, 0)";
+$sql = "INSERT INTO cCommunity (cmtyTitle, cmtyContent, cmtyNic, cmtyPasswd, cmtyDate, cmtyImg, cmtyLikeCnt) VALUES ('$title', '$contents', '$name', '$password', NOW(), '$dstName', 0)";
 
 // 데이터 삽입 실행
 if ($db->query($sql) === TRUE) {
-   
+    echo "데이터가 성공적으로 삽입되었습니다.";
 } else {
     echo "데이터 삽입 중 오류가 발생하였습니다: " . $db->error;
 }
